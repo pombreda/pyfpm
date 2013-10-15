172d742e4431ce54a55a2bff32a8344b707e3792
Pingax <anthony@pingax.no-ip.info>
2013-10-15 21:31:41 +0200

    Update of src/Functions/system.py : - Now use a dictionary to store informations

--- src/Functions/system.py
+++ src/Functions/system.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf-8 *-*
 
+# ----------------------------------------------------------------------
+#
+#       Permet de récupérer certaines informations système
+#
+# ----------------------------------------------------------------------
 
-import platform, os
+import platform, os, subprocess, re, package
 from datetime import timedelta
-import psutil
 
-print ("%s@%s") % (os.getlogin(), platform.node())
+Package = package.Package()
 
-print ("Kernel : %s %s %s") % (platform.machine(), platform.uname()[0], platform.uname()[2])
+sysinfos = {}
+
+sysinfos["user"] = os.getlogin() + "@" + platform.node()
+
+sysinfos["kernel"] = str("%s %s %s") % (platform.machine(), platform.uname()[0], platform.uname()[2])
 
 with open('/proc/uptime', 'r') as f:
 	uptime_seconds = float(f.readline().split()[0])
 	uptime_string = str(timedelta(seconds = uptime_seconds))
 
-print ("Uptime : %s" % uptime_string)
+sysinfos["uptime"] = uptime_string
+
+''' Workaround as platform.processor() returns empty string '''
+command = "cat /proc/cpuinfo"
+all_info = subprocess.check_output(command, shell=True).strip()
+for line in all_info.split("\n"):
+	if "model name" in line:
+		sysinfos["proc"] = re.sub(".*model name.*:", "", line, 1)
+
+sysinfos["nb_pkgs"] = len(Package.getInstalledList())
